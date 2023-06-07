d5e3f07840e3093a38459788cc8f1639fe8896b4
JohnJohn21121 <john.abreu21121@gmail.com>
2023-06-07 20:55:36 -0300

    added

--- Functional Programming/src/main/java/org/example/Main.java
+++ Functional Programming/src/main/java/org/example/Main.java
@@ -4,9 +4,9 @@ import org.example.abstractpractice.Cat;
 
 public class Main {
     public static void main(String[] args) {
-        Cat cat = new Cat("Orugo", 1);
+        Cat cat = new Cat("Orugo", 1,"Raza");
         cat.makeNoise();
         System.out.println("Hello world! let me show you my cat he is " + cat.getName() +
-                " , and he is " + cat.getAge() + " years old.");
+                " , and he is " + cat.getAge() + " years old. He loves to eat " + cat.getFoodOfPreference());
     }
 }
