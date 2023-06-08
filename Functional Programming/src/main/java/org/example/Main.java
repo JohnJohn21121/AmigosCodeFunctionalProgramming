3c71676a1d19f4f7fe343def0e5a06c32a4405f7
JohnJohn21121 <john.abreu21121@gmail.com>
2023-06-08 15:04:55 -0300

    added some modifications to the classees and imports also added the practice in Main.java for Optionals with functions

--- Functional Programming/src/main/java/org/example/Main.java
+++ Functional Programming/src/main/java/org/example/Main.java
@@ -1,12 +1,29 @@
 package org.example;
 
 import org.example.abstractpractice.Cat;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        Cat cat = new Cat("Orugo", 1,"Raza");
+        Cat cat = new Cat("Orugo", 1, "Raza");
         cat.makeNoise();
         System.out.println("Hello world! let me show you my cat he is " + cat.getName() +
                 " , and he is " + cat.getAge() + " years old. He loves to eat " + cat.getFoodOfPreference());
+
+
+        Object value = Optional.ofNullable("Hello").orElseThrow(() -> new IllegalStateException("Yikes "));
+        System.out.println(value);
+
+        Optional.ofNullable("johnjohn21121@gmail.com")
+                //usage of ifPresentOrelse to properly point that we need to pass as a second action a runnable.
+                .ifPresentOrElse(email -> System.out.println("Sending email to " + email), () -> {
+                    System.out.println("cannot Send Email");
+                });
+        Optional.ofNullable(null)
+                //usage of ifPresentOrelse to properly point that we need to pass as a second action a runnable.
+                .ifPresentOrElse(email -> System.out.println("Sending email to " + email), () -> {
+                    System.out.println("cannot Send Email");
+                });
+
     }
 }
