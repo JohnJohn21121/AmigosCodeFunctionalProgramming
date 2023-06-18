b5a17d23e13cdf44664e92e77f842794a8bff0e9
JohnJohn21121 <john.abreu21121@gmail.com>
2023-06-18 14:08:24 -0300

    added Multithreading practices

---
+++ Functional Programming/src/main/java/org/example/multithreading/MultithreadThingImplements.java
@@ -0,0 +1,24 @@
+package org.example.multithreading;
+
+public class MultithreadThingImplements implements Runnable {
+    private int threadNumber;
+
+    public MultithreadThingImplements(int threadNumber) {
+        this.threadNumber = threadNumber;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(i + " from thread " + threadNumber);
+            if (threadNumber == 3) {
+                throw new RuntimeException();
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
