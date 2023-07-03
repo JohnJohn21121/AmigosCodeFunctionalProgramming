53498c8cfe6dbf8b94212b8bf26f92170acb39a1
JohnJohn21121 <john.abreu21121@gmail.com>
2023-07-03 20:54:18 -0300

    Added UserInput

--- CodeDex/PythonTraining/Beginner/DataTypes.py
+++ CodeDex/PythonTraining/Beginner/DataTypes.py
@@ -3,4 +3,16 @@ user_id = 16180339887
 progress = 0.75
 exp = 60
 verified = True
-print('-------------------------------')
+#Mainly the good old DataTypes that we all know
+
+# Small Formula to convert F° to C° where 84 is the current Wheater
+celcius = (84-32)/1.8
+print(celcius)
+
+# Bmi Formula is mass / height^2 
+print(70 / (1.72**2))
+# Input() functions takes user input
+print('-----------------')
+username = input('Enter your name: ')
+
+print('Hi '+ username+ ', you are doing this on python')
